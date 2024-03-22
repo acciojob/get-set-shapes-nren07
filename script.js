d127ddecee9ab76c1641c047b261674765079382
NARENDRA SINGH <117838092+nren07@users.noreply.github.com>
2024-03-22 08:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this.width=width;
+		this.height=height;
+	}
+	getWidth(){
+		return this.width;
+	}
+	getHeight(){
+		return this.height;
+	}
+	setWidth(width){
+		this.width=width;
+	}
+	setHeight(height){
+		this.height=height;
+	}
+	getArea(){
+		return this.height*this.width;
+	}
+}
 
-class Square extends Animal {}
-
+class Square extends Rectangle {
+	constructor(side){
+		super(side,side);
+	}
+	getPerimeter(){
+		return 2*(this.width+this.height);
+		
+	}
+}
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
